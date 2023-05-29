b091c0c68b6f81f68aca39b6ab711fed3d5d8c1b
JhoannPV <91164583+JhoannPV@users.noreply.github.com>
2023-05-28 19:25:03 -0500

    firts commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import NavbarUse from "./components/NavbarUse";
+
+function App() {
+  return (
+    <>
+      <NavbarUse />
+      <span className="material-symbols-outlined">search</span>
+      <br />
+      <i className="bi bi-apple"></i>
+    </>
+  );
+}
+
+export default App;
