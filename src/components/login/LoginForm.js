2345f9a19a46b23f104c215989cb195e02552c69
JhoannPV <91164583+JhoannPV@users.noreply.github.com>
2023-05-31 23:07:54 -0500

    toast

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -1,28 +1,64 @@
-import { useNavigate } from "react-router-dom";
 import "./LoginForm.css";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
 
 function LoginForm() {
-  const navigate = useNavigate();
+  let mensajeAviso;
   let ruta = "/plantillas";
-  function handleClick() {
-    navigate(ruta);
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  function login(username, password) {
+    if (username === "Jhoann" && password === "root") {
+      mensajeAviso = "Usuario Autenticado";
+      toast.done(mensajeAviso);
+      navigate(ruta);
+    } else {
+      if (username !== "Jhoann") {
+        mensajeAviso = "Usuario no registrado";
+      } else if (username === "Jhoann" && password !== "root") {
+        mensajeAviso = "Contraseña incorrecta";
+      }
+      toast.error(mensajeAviso);
+    }
   }
+
   return (
     <>
       <div className="login">
-        <Form>
+        <ToastContainer />
+        <Form
+          onSubmit={(ev) => {
+            ev.preventDefault();
+            login(username, password);
+          }}
+        >
           <Form.Group className="mb-3" controlId="formBasicUsername">
             <Form.Label>Nombre se Usuario</Form.Label>
-            <Form.Control type="text" placeholder="Ingrese Nombre de Usuario" />
+            <Form.Control
+              type="text"
+              placeholder="Ingrese Nombre de Usuario"
+              required
+              autoComplete="off"
+              name="username"
+              onChange={(ev) => setUsername(ev.target.value)}
+            />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Contraseña</Form.Label>
-            <Form.Control type="password" placeholder="Contraseña" />
+            <Form.Control
+              type="password"
+              placeholder="Contraseña"
+              required
+              name="password"
+              onChange={(ev) => setPassword(ev.target.value)}
+            />
           </Form.Group>
           <div className="centrar-button">
-            <Button variant="primary" type="submit" onClick={handleClick}>
+            <Button variant="primary" type="submit">
               SignIn
             </Button>
           </div>
