b0b7ee1fdf062a70698cde54c31cdf739c4a928e
JhoannPV <91164583+JhoannPV@users.noreply.github.com>
2023-06-01 21:52:17 -0500

    validar usuario

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -4,38 +4,33 @@ import Form from "react-bootstrap/Form";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
+import axios from "axios";
 
 function LoginForm() {
-  let mensajeAviso;
-  let ruta = "/plantillas";
+  let rutaPlan = "/plantillas";
   const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  function login(username, password) {
-    if (username === "Jhoann" && password === "root") {
-      mensajeAviso = "Usuario Autenticado";
-      toast.done(mensajeAviso);
-      navigate(ruta);
-    } else {
-      if (username !== "Jhoann") {
-        mensajeAviso = "Usuario no registrado";
-      } else if (username === "Jhoann" && password !== "root") {
-        mensajeAviso = "Contraseña incorrecta";
-      }
-      toast.error(mensajeAviso);
-    }
-  }
 
+  const handleSubmit = async (ev) => {
+    ev.preventDefault();
+    let credentials = { username, password };
+    try {
+      const url = "http://localhost:3001/api/v1/auth/signin";
+      const res = await axios.post(url, credentials);
+      const token = res.data.data.token;
+      localStorage.setItem("jwt-token", token);
+      console.log(localStorage.getItem("jwt-token"));
+      navigate(rutaPlan);
+    } catch (error) {
+      toast.error("Falló: " + error.message);
+    }
+  };
   return (
     <>
       <div className="login">
         <ToastContainer />
-        <Form
-          onSubmit={(ev) => {
-            ev.preventDefault();
-            login(username, password);
-          }}
-        >
+        <Form onSubmit={handleSubmit}>
           <Form.Group className="mb-3" controlId="formBasicUsername">
             <Form.Label>Nombre de Usuario</Form.Label>
             <Form.Control
