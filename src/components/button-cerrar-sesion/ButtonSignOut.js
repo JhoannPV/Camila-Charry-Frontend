2bdb559a896b50914cb7b611ecc9889f25c9f16a
JhoannPV <91164583+JhoannPV@users.noreply.github.com>
2023-06-02 07:49:11 -0500

    fonts

---
+++ src/components/button-cerrar-sesion/ButtonSignOut.js
@@ -0,0 +1,12 @@
+import "./ButtonSignOut.css";
+import Button from "react-bootstrap/Button";
+
+function ButtonSignOut() {
+  return (
+    <>
+      <Button variant="danger">Danger</Button>
+    </>
+  );
+}
+
+export default ButtonSignOut;
